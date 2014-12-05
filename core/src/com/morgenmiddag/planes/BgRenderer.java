da9edeb9aca0ab7e0d7a274df8348bf05b49a61a
Yuri de Snaijer <yuridesnaijer@gmail.com>
2014-12-06 00:42:07 +0100

    added infinite background scrolling

--- core/src/com/morgenmiddag/planes/BgRenderer.java
+++ core/src/com/morgenmiddag/planes/BgRenderer.java
@@ -17,37 +17,55 @@ public class BgRenderer {
     private Texture bg1;
     private Sprite cloud;
     private Array<Vector2> cloudArray = new Array<Vector2>();
-    private Vector2 bg1Position;
+    private Array<Sprite> backgroundArray = new Array<Sprite>();
+//    private Vector2 bg1Position;
     private int speed = 2;
     private Random r = new Random();
 
     public BgRenderer(){
         bg1 = new Texture("bg1.png");
-        bg1Position = new Vector2(0, 0);
+//        bg1Position = new Vector2(0, 0);
+
+        for (int i = 0; i < 2; i++){
+            backgroundArray.add(new Sprite(bg1, 0, 0, bg1.getWidth(), bg1.getHeight()));
+            backgroundArray.get(i).setPosition(i * bg1.getWidth(), 0);
+        }
+
         cloud = new Sprite(new Texture("cloud.png"));
     }
 
     public void Draw(SpriteBatch batch){
-        batch.draw(bg1, bg1Position.x, bg1Position.y);
+
+        for (int i = 0; i < backgroundArray.size; i++){
+            backgroundArray.get(i).draw(batch);
+        }
+        //batch.draw(bg1, bg1Position.x, bg1Position.y);
+
         for (int i = 0; i < cloudArray.size; i++){
             batch.draw(cloud, cloudArray.get(i).x, cloudArray.get(i).y);
         }
     }
 
     public void Update(){
-        bg1Position.x -= speed;
 
-        if (bg1Position.x < -1920){
-            bg1Position.x = 0;
+        for (int i = 0; i < backgroundArray.size; i++){
+            Sprite bg = backgroundArray.get(i);
+            float x = bg.getX();
+            bg.setX(x -= speed);
+
+            if (bg.getX() + bg.getWidth() < 1){
+                bg.setX(i * bg.getWidth() + bg.getWidth());
+            }
         }
+
+//        bg1Position.x -= speed;
+//
+//        if (bg1Position.x < -1920){
+//            bg1Position.x = 0;
+//        }
 
         CreateCloud();
-    }
 
-    public void CreateCloud(){
-        if(r.nextInt(1000) < 2){
-            cloudArray.add(new Vector2(Gdx.graphics.getWidth() + 10 ,0));
-        }
         if(cloudArray.size > 0){
             for (int i = 0; i < cloudArray.size; i++){
                 if(cloudArray.get(i).x < 0 - cloud.getWidth()){
@@ -56,6 +74,12 @@ public class BgRenderer {
                     cloudArray.get(i).set(cloudArray.get(i).x -= 1.5f, cloudArray.get(i).y += 0.2f);
                 }
             }
+        }
+    }
+
+    public void CreateCloud(){
+        if(r.nextInt(1000) < 2){
+            cloudArray.add(new Vector2(Gdx.graphics.getWidth() + 10 ,0));
         }
 
     }
